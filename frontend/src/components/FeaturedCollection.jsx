0d90afe1d1532b12c9dc1ccd449c5a163cfb0a85
ravitej <itsmeravitej05@gmail.com>
2026-03-12 10:58:40 +0530

    bug: solved a react bug

---
+++ frontend/src/components/FeaturedCollection.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+
+const FeaturedCollection = () => {
+    const {products} = useContext(ShopContext);
+    console.log(products);
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default FeaturedCollection
